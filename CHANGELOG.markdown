eb161d6bc3c9e4b8183888af19755bbe2a0720cc
Matvey Aksenov <matvey.aksenov@gmail.com>
2014-11-07 19:30:16 +0000

    `Eq`, `Foldable`, `Traversable`

---
+++ CHANGELOG.markdown
@@ -0,0 +1,6 @@
+0.3.0
+=====
+
+  * Windows support (https://github.com/biegunka/terminal-size/pull/4)
+
+  * Added `Eq`, `Foldable` and `Traversable` instances for `Window`
--- src/System/Console/Terminal/Common.hs
+++ src/System/Console/Terminal/Common.hs
@@ -3,11 +3,22 @@ module System.Console.Terminal.Common
   ( Window(..)
   ) where
 
+import Control.Applicative
+import Data.Foldable (Foldable(..))
+import Data.Traversable (Traversable(..))
+import Data.Monoid (mappend)
+
 -- | Terminal window width and height
 data Window a = Window
   { height :: !a
   , width  :: !a
-  } deriving (Show, Read)
+  } deriving (Show, Eq, Read)
 
 instance Functor Window where
   fmap f (Window { height = h, width = w }) = Window { height = f h, width = f w }
+
+instance Foldable Window where
+  foldMap f (Window { height = h, width = w }) = f h `mappend` f w
+
+instance Traversable Window where
+  traverse f (Window { height = h, width = w }) = Window <$> f h <*> f w
