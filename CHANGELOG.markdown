97292c9510c86a3dd614714f7c96fc72b864c0ca
Matvey Aksenov <matvey.aksenov@gmail.com>
2015-11-01 15:01:37 +0000

    Version bump and changelog entry.

--- CHANGELOG.markdown
+++ CHANGELOG.markdown
@@ -1,3 +1,8 @@
+0.3.2.1
+=======
+
+  * GHC 7.2 support (https://github.com/biegunka/terminal-size/pull/9)
+
 0.3.2
 =====
 
--- package.nix
+++ package.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, stdenv }:
 mkDerivation {
   pname = "terminal-size";
-  version = "0.3.2";
+  version = "0.3.2.1";
   src = ./.;
   buildDepends = [ base ];
   description = "Get terminal window height and width";
